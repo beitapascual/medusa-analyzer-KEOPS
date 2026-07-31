789eb9b91b60e60acd482d722c6f47228d80ec76
vicvrg <victor.rodriguez@uva.es>
2026-07-31 10:26:28 +0200

    Run pipeline is almost here :D

---
+++ medusa_analyzer/frontend/experiments/eeg/widgets/eeg_run_experiment_widget.py
@@ -0,0 +1,38 @@
+from medusa_analyzer.frontend.widgets.run_experiment import RunExperimentWidget
+from medusa_analyzer.frontend.worker import Worker
+from medusa_analyzer.backend.experiments.eeg_run_pipeline import run_eeg_feature_extraction
+
+class EEGRunExperimentWidget(RunExperimentWidget):
+    """Run step específico para el experimento de conversión."""
+
+    def __init__(self, experiment_info: dict, defaults: dict, state: dict):
+        # Inicializa la clase padre genérica con los parámetros necesarios
+        super().__init__(experiment_info, defaults, state)
+
+    def run_pipeline(self):
+        """Start the converter pipeline in a background worker."""
+        if self.pipeline_running:
+            return
+
+        self.pipeline_running = True
+        self.state["completion_status"] = "incompleted"
+        self.changed.emit()
+
+        self.status_label.setText(f"Running {self.experiment_info['title']}...")
+        self.clear_logs()
+        self.log_callback(f"Starting {self.experiment_info['title']}...")
+        self.set_progress(0)
+
+        kwargs = {
+            "state": self.state,
+            "progress_callback": self.set_progress,
+            "log_callback": self.log_callback
+        }
+
+        worker = Worker(run_eeg_feature_extraction, **kwargs)
+        worker.signals.progress.connect(self.set_progress)
+        worker.signals.logging.connect(self.log_callback)
+        worker.signals.result.connect(self._pipeline_completed)
+        worker.signals.error.connect(self._pipeline_failed)
+        worker.signals.finished.connect(self._pipeline_finished)
+        self.runner.start(worker)
